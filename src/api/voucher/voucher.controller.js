e489d937806b21c6dc1762fdb9943aa8fa86e482
guardezi <jefersonguardezi@gmail.com>
2018-05-03 10:34:43 -0300

    ajuste no vouhcer

--- src/api/voucher/voucher.controller.js
+++ src/api/voucher/voucher.controller.js
@@ -4,7 +4,6 @@ import { findVoucherByCode } from './voucher.services'
 export async function findVoucher (req, res) {
   try {
     let voucher = await findVoucherByCode(req.params.code)
-
     res.json(voucher)
   } catch (e) {
     res.status(httpStatus.BAD_REQUEST).json(e)
